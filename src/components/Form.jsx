1832509a26431228c62cc924ae20691a3cd45473
shyam <snandan781@gmail.com>
2023-07-05 13:43:45 +0530

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,47 @@
+import { Box, InputBase, Button, styled } from "@mui/material";
+import { useState } from "react";
+import { getWeather } from "../services/api";
+
+const Container = styled(Box)({
+  background: "#445A6F",
+  padding: 10,
+});
+
+const Input = styled(InputBase)({
+  color: "#FFF",
+  marginRight: "20px",
+  fontSize: "18",
+});
+
+const GetButton = styled(Button)({
+  background: "#e67e22",
+  width: "150",
+});
+
+const Form = ({ setResult }) => {
+  const [data, setData] = useState({ city: "", country: "" });
+
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  const getWeatherInfo = async () => {
+    let response = await getWeather(data.city, data.country);
+    setResult(response);
+  };
+  return (
+    <Container>
+      <Input placeholder="City" onChange={(e) => handleChange(e)} name="city" />
+      <Input
+        placeholder="Country"
+        onChange={(e) => handleChange(e)}
+        name="country"
+      />
+      <GetButton variant="contained" onClick={() => getWeatherInfo()}>
+        Get Weather
+      </GetButton>
+    </Container>
+  );
+};
+
+export default Form;
